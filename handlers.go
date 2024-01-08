d31b30d1d538869ca09088f8db173181a52db0a7
Remington Cunov <61329286+rcunov@users.noreply.github.com>
2024-01-08 01:42:06 -0600

    move handlers to new file

--- handlers.go
+++ handlers.go
@@ -1,32 +1,12 @@
 package main
 
-// Import dependencies
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"os"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 )
-
-// Class for albums
-type Album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-// Test data
-var albumPersistentStorage = []Album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Bleed the Future", Artist: "AUM", Price: 19.99},
-	{ID: "3", Title: "Super Hexagon", Artist: "Chipzel", Price: 8.0},
-	{ID: "4", Title: "Hirschbrunnen", Artist: "delving", Price: 14.99},
-}
 
 // Get everything from persistent storage
 func getAllAlbums(c *gin.Context) {
@@ -83,38 +63,4 @@ func isValidPort(portStr string) bool {
 	}
 
 	return false
-}
-
-func main() {
-	// Set release mode
-	gin.SetMode(gin.ReleaseMode)
-
-	// Instantiate router
-	var router = gin.Default()
-
-	// Define API endpoints
-	router.GET("/albums", getAllAlbums)
-	router.POST("/upload", uploadOneOrManyAlbums)
-	// TODO: Add GET method for single record
-	// TODO: https://go.dev/doc/tutorial/web-service-gin#specific_item
-
-	// Disable proxy warning message
-	router.SetTrustedProxies(nil)
-	// Example way to set trusted proxies if I change my mind
-	// // router.SetTrustedProxies([]string{"192.168.1.2"})
-
-	// Listen on any address using custom port if set by user, defaulting to port 8117 if not set
-	listenPort, isSet := os.LookupEnv("listenPort") // Check if $listenPort is set and get value if so
-	if isSet {
-		isValid := isValidPort(listenPort) // Check if it's a valid port number
-		if isValid {
-			listenAddr := fmt.Sprintf("0.0.0.0:%s", listenPort)
-			router.Run(listenAddr)
-		} else {
-			log.Fatalf("ERROR! listenPort is invalid. Currently set to: %s", listenPort)
-			// I don't like the default log output of this, but it's kind of a pain to change. Maybe later
-		}
-	} else {
-		router.Run("0.0.0.0:8117")
-	}
 }
