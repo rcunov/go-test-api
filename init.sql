d067fbc1740fae8f5681a0eb0f2c1a08556c2a90
Remington Cunov <61329286+rcunov@users.noreply.github.com>
2024-01-10 16:26:43 -0600

    add database init script

---
+++ init.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    title VARCHAR NOT NULL, 
+    artist VARCHAR NOT NULL, 
+    price DECIMAL(5,2) NOT NULL
+);
+
+INSERT INTO albums
+  (title, artist, price)
+VALUES
+  	('Blue Train', 'John Coltrane', 56.99),
+	('Bleed the Future', 'AUM', 19.99),
+	('Super Hexagon', 'Chipzel', 8.0),
+	('Hirschbrunnen', 'delving', 14.99);
+
+-- .mode column
+-- .headers on
